320d41c07ac1562e25565adb5a39e65aa2eb8d0f
zhang yi <intotherainzy@gmail.com>
2013-11-05 08:38:29 +0800

    Initial Commit

---
+++ YantaiO/JDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JDAppDelegate.h
+//  YantaiO
+//
+//  Created by zhang yi on 13-11-5.
+//  Copyright (c) 2013年 胶东在线. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YantaiO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YantaiO
+//
+//  Created by zhang yi on 13-11-5.
+//  Copyright (c) 2013年 胶东在线. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JDAppDelegate class]));
+    }
+}
